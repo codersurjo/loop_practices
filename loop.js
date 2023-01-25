74f31b01c50d495999c9ba42c3a6becd169f2ffc
codersurjo <codersurjo@gmail.com>
2023-01-25 22:43:02 +0600

    While loop commit

---
+++ loop.js
@@ -0,0 +1,8 @@
+var roastGiven = 0;
+while(roastGiven < 7){
+    console.log("Roast den !!");
+    console.log(roastGiven);
+    // roastGiven = roastGiven + 1;
+    // roastGiven +=1;
+    roastGiven++;
+}
