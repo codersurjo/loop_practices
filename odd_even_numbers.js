839c5cdd5229770cba929c7ac709ee8e1d05ed87
codersurjo <codersurjo@gmail.com>
2023-01-25 22:54:32 +0600

    odd even js

---
+++ odd_even_numbers.js
@@ -0,0 +1,25 @@
+
+//Display 1 to 7 numbers using loop
+var number = 1;
+while(number <= 7){
+    console.log(number);
+    number++;
+}
+
+//even numbers
+ 
+var numbers = 0;
+
+while(numbers < 100){
+    console.log(numbers);
+    numbers+=2;
+}
+
+
+// odd numbers
+var numbers = 1;
+
+while(numbers < 100){
+    console.log(numbers);
+    numbers+=2;
+}
